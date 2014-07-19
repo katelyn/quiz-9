84ccb7e2788b7e73d23d13c3f30d5989bc01d46e
Conrad Benham <conrad@agilehongkong.com>
2014-07-19 13:23:31 -0400

    Calculating the duration of the seconds from a duration string.

--- lib/ext/string.rb
+++ lib/ext/string.rb
@@ -1,5 +1,10 @@
 class String
+
+  DURATION_FORMAT = Regexp.new('^(?<hours>\d{3}):(?<minutes>\d{2}):(?<seconds>\d{2})$')
+
   def duration_to_seconds
-    0
+    match_data = DURATION_FORMAT.match(self)
+    raise "#{self} is not a valid duration" unless match_data
+    match_data['seconds'].to_i
   end
 end
--- spec/ext/string_spec.rb
+++ spec/ext/string_spec.rb
@@ -1,8 +1,16 @@
 describe String do
 
   describe '#duration_to_seconds' do
+    it 'should raise an exception when the format is not recognized' do
+      expect { 'not_a_duration'.duration_to_seconds }.to raise_error 'not_a_duration is not a valid duration'
+    end
+
     it 'should have a zero duration for 000:00:00' do
       expect('000:00:00'.duration_to_seconds).to equal(0)
+    end
+
+    it 'should have a count of the number of seconds for the seconds field' do
+      expect('000:00:25'.duration_to_seconds).to equal(25)
     end
   end
 
