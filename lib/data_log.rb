1a0a9fc16c49029e85002819e6bc9ea246648b35
Conrad Benham <conrad@agilehongkong.com>
2014-07-19 16:29:30 -0400

    Created majority of the report.

--- lib/data_log.rb
+++ lib/data_log.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class DataLog
 
   def initialize(array_of_hash_like_data)
@@ -5,29 +7,39 @@ class DataLog
   end
 
   def row_count
-    @data.count
+    data.count
   end
 
   def call_dest_with_value_count(search_value)
-    @data.inject(0) { |count, row| row['call-dest'] == search_value ? count + 1 : count }
+    data.inject(0) { |count, row| row['call-dest'] == search_value ? count + 1 : count }
   end
 
   def call_dest_with_unique_values
     result = Hash.new { |hash, key| hash[key] = 0 }
-    @data.each { |row| result[row['call-dest']] += 1 }
+    data.each { |row| result[row['call-dest']] += 1 }
     result
   end
 
   def  total_call_duration
-    @data.inject(0) { |count, row| row['call-duration'].duration_to_seconds + count }
+    data.inject(0) { |count, row| row['call-duration'].duration_to_seconds + count }
   end
 
   def report
+    unique_pairs = call_dest_with_unique_values
+    unique_call_dest_values_for_reporting = unique_pairs.keys.sort.map { |ip| "#{ip}=#{unique_pairs[ip]}" }.join('; ')
+
     'Data Report:' +
         "\n\t1: #{row_count}" +
         "\n\t2: #{call_dest_with_value_count('208.51.154.200')}" +
-        "\n\t3: #{call_dest_with_unique_values}" +
+        "\n\t3: #{unique_call_dest_values_for_reporting}" +
         "\n\t4: #{total_call_duration}"
+  end
+
+  private
+
+  def data
+    @data.rewind if @data.instance_of?(CSV)
+    @data
   end
 
 end
--- spec/acceptance_spec.rb
+++ spec/acceptance_spec.rb
@@ -2,11 +2,11 @@ require 'csv'
 
 describe 'Quiz Output' do
 
-  xit 'should have a report with all requested information' do
-    csv = CSV.new('../data/quiz1.csv', col_sep: ';', headers: true)
+  it 'should have a report with all requested information' do
+    csv = CSV.open("#{Dir.pwd}/data/quiz1.csv", col_sep: ';', headers: true)
     data_log = DataLog.new(csv)
 
-    expect(data_log.report).to equal(expected_data_log_report)
+    expect(data_log.report).to eq(expected_data_log_report)
   end
 
   private
@@ -16,9 +16,7 @@ describe 'Quiz Output' do
         "\n\t1: 46" +
         "\n\t2: 6" +
         "\n\t3: 208.37.201.215=12; 208.51.154.200=6; 208.69.83.142=2; 67.211.162.224=4; 69.44.55.132=4; 72.35.229.246=8; 76.10.192.169=10" +
-        "\n\t4: 18226" +
-        "\n\n* In the answer for question 1, the total number of rows excludes both the header row and also the final" +
-        ' line break as that creates an empty line which has been assumed not to be a row'
+        "\n\t4: 18226"
   end
 
 end
