e69c86c4e8beba08777d745e68bb009409ad84ce
Conrad Benham <conrad@agilehongkong.com>
2014-07-19 13:33:51 -0400

    Calculating the number duration and adding hours, minutes and seconds together.

--- lib/ext/string.rb
+++ lib/ext/string.rb
@@ -1,6 +1,7 @@
 class String
 
   DURATION_FORMAT = Regexp.new('^(?<hours>\d{3}):(?<minutes>\d{2}):(?<seconds>\d{2})$')
+  MINUTES_PER_HOUR = 60
   SECONDS_PER_MINUTE = 60
 
 
@@ -9,6 +10,7 @@ class String
     raise "#{self} is not a valid duration" unless match_data
     seconds = match_data['seconds'].to_i
     minutes_in_seconds = match_data['minutes'].to_i * SECONDS_PER_MINUTE
-    seconds + minutes_in_seconds
+    hours_in_minute = match_data['hours'].to_i * MINUTES_PER_HOUR * SECONDS_PER_MINUTE
+    seconds + minutes_in_seconds + hours_in_minute
   end
 end
--- spec/ext/string_spec.rb
+++ spec/ext/string_spec.rb
@@ -13,8 +13,16 @@ describe String do
       expect('000:00:03'.duration_to_seconds).to equal(3)
     end
 
-    it 'should count the number of seconds for the minutes field and add it to the seconds field' do
-      expect('000:02:04'.duration_to_seconds).to equal(124)
+    it 'should count the number of seconds for the minutes field' do
+      expect('000:02:00'.duration_to_seconds).to equal(120)
+    end
+
+    it 'should count the number of seconds for the hours field' do
+      expect('012:00:00'.duration_to_seconds).to equal(43200)
+    end
+
+    it 'should add all fields of the duration together' do
+      expect('020:34:28'.duration_to_seconds).to equal(74068)
     end
   end
 
