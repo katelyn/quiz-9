1f06d70bcca0f82981a3c4e12125811c65c14bdd
Conrad Benham <conrad@agilehongkong.com>
2014-07-20 13:20:48 -0400

    Extracting path of report so it may be executed from any directory.

--- report.rb
+++ report.rb
@@ -1,5 +1,7 @@
-require './lib/data_log.rb'
-require './lib/ext/string.rb'
+current_file_path = File.expand_path(File.dirname(__FILE__))
+
+require "#{current_file_path}/lib/data_log.rb"
+require "#{current_file_path}/lib/ext/string.rb"
 require 'csv'
 
-puts DataLog.new(CSV.open('./data/quiz1.csv', col_sep: ';', headers: true)).report
+puts DataLog.new(CSV.open("#{current_file_path}/data/quiz1.csv", col_sep: ';', headers: true)).report
